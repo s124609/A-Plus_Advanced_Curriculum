8e2badb58ce9986cdd826a30452e41f42917e7ad
s124609 <seth.hancock609@student.mckinneyisd.net>
2018-09-18 10:51:27 -0500

    lab08 Maps added

---
+++ lab08_Maps/src/PartRunner.java
@@ -0,0 +1,10 @@
+import static java.lang.System.*;
+
+public class PartRunner
+{
+    public static void main(String[] args)
+    {
+        PartList prog = new PartList("partinfo.dat");
+        out.println(prog);
+    }
+}
