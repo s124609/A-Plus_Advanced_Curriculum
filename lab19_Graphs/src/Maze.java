fb8fcf226227f9482122c253220fc088ed22e589
s124609 <seth.hancock609@student.mckinneyisd.net>
2019-01-17 12:46:44 -0600

    Modified for Battlecode

--- lab19_Graphs/src/Maze.java
+++ lab19_Graphs/src/Maze.java
@@ -1,18 +1,30 @@
+import java.lang.*;
+import java.time.Duration;
+import java.time.Instant;
+
 public class Maze
 {
     private int[][] maze;
+    private boolean[][] pasable;
+    private long start_time;
 
-    Maze(int[][] m) {
-        this.maze = m;
+    Maze(boolean[][] m) {
+        this.pasable = m;
+        this.maze = new int[m.length][m.length];
+        maze[9][5] = 647;
+        start_time = System.nanoTime();
     }
 
     private boolean checkForExitPath(int r, int c) {
-        if (r > -1 && c > -1 && r < this.maze.length && c < this.maze.length && this.maze[r][c] == 1) {
-            this.maze[r][c] = -1;
-            if (c == this.maze.length - 1) {
+
+        if (r > -1 && c > -1 && r < this.pasable.length && c < this.pasable.length
+                && maze[r][c] > -1 && this.pasable[r][c]) {
+            if (maze[r][c] == 647) {
                 return true;
             } else {
-                return this.checkForExitPath(r - 1, c) || this.checkForExitPath(r, c + 1) || this.checkForExitPath(r + 1, c) || this.checkForExitPath(r, c - 1);
+                this.maze[r][c] = -1;
+                return this.checkForExitPath(r - 1, c) || this.checkForExitPath(r, c + 1)
+                        || this.checkForExitPath(r + 1, c) || this.checkForExitPath(r, c - 1);
             }
         } else {
             return false;
@@ -23,8 +35,19 @@ public class Maze
         String output;
         if (this.checkForExitPath(0, 0)) {
             output = "exit found\n";
+
+            long end_time = System.nanoTime();
+            double difference = (end_time - start_time) / 1e6;
+
+            System.out.println(difference + "ms");
         } else {
             output = "exit not found\n";
+        }
+
+        for (int i = 0; i < maze.length; i++) {
+            for (int x = 0; x < maze.length; x++)
+                output += "" + maze[i][x];
+            output += "\n";
         }
 
         return output;
--- lab19_Graphs/src/MazeRunner.java
+++ lab19_Graphs/src/MazeRunner.java
@@ -6,7 +6,7 @@ public class MazeRunner
 {
     public static void main(String[] args) throws IOException
     {
-        Scanner input = new Scanner(new File("lab19_Graphs/Maze.dat"));
+        Scanner input = new Scanner(new File("lab19_Graphs/passable.dat"));
 
         while (input.hasNextInt()) {
             int size = input.nextInt();
@@ -21,9 +21,25 @@ public class MazeRunner
                 System.out.println();
             }
 
-            Maze test = new Maze(maze);
+
+            boolean[][] passable = parseBoolean(maze);
+            Maze test = new Maze(passable);
             System.out.println(test);
         }
 
+    }
+
+    private static boolean[][] parseBoolean(int[][] maze)
+    {
+        boolean[][] passable = new boolean[maze.length][maze.length];
+        for (int i = 0; i < maze.length; i++) {
+            for (int x = 0; x < maze.length; x++)
+                if (maze[i][x] == 1)
+                    passable[i][x] = true;
+                else
+                    passable[i][x] = false;
+        }
+
+        return passable;
     }
 }
